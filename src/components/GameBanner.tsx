7d29ed54f107de00d5894e7197d1ee6458ba245b
Felipe <felipetorta@gmail.com>
2023-01-16 09:00:57 -0300

    Fix: Responsive banners

--- src/components/GameBanner.tsx
+++ src/components/GameBanner.tsx
@@ -8,7 +8,7 @@ interface GameBannerProps {
 
 export function GamerBanner(props: GameBannerProps) {
   return (
-      <div className="relative rounded-lg overflow-hidden">
+      <div className="relative rounded-lg overflow-hidden h-[180px] md:h-64">
         <img src={props.bannerUrl} alt="" />
 
         <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
